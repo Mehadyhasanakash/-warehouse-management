0e0eb2ac3c207f03746e666032bfd5cc7fe7f634
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2022-05-01 21:24:00 +0600

    home

--- src/Components/Nav/Naver.js
+++ src/Components/Nav/Naver.js
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 import './Nav.css'
 const Naver = () => {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="success" expand="lg">
         <Container>
           <Navbar.Brand as={Link} to="/home"><img className='img'  src="https://i.ibb.co/8XRpCC8/logo01.png" alt="" /></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link as={Link} to="/home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link className='text-light' as={Link} to="/home">Home</Nav.Link>
+              
               
             </Nav>
           </Navbar.Collapse>
